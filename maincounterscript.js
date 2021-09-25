0d2445e2b32e4839bc3ed2ee4acb3afe74dd4f2b
Software Engineer Tapesh Vasisth | सॉफ्टवेयर डेवलपर तपेश वशिष्ठ <83491505+SoftwareEngineerTapeshVasisth@users.noreply.github.com>
2021-09-25 07:33:47 +0530

    Add files via upload
    Ghhh

---
+++ maincounterscript.js
@@ -0,0 +1,34 @@
+   $('.counter').counterUp({
+  time:5000
+});
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    speed: 300,
+    infinite: true,
+    asNavFor: '.slider-nav-thumbnails',
+    autoplay:true,
+    pauseOnFocus: true,
+    dots: true,
+  });
+ 
+  $('.slider-nav-thumbnails').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    focusOnSelect: true,
+    infinite: true,
+    prevArrow: false,
+    nextArrow: false,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+        }
+      }
+    ]
+  });
